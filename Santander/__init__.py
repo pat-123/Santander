b52d4c1f4896d2c7226f6c134b2120b87caf2486
pat@123 <thapliyalpooja69@gmail.com>
2019-09-11 16:51:25 +0530

    cleanup

--- Santander/__init__.py
+++ Santander/__init__.py
@@ -5,13 +5,12 @@ Created on Tue Aug 13 13:38:47 2019
 @author: PAT
 """
 
+import warnings
+warnings.filterwarnings("ignore")
 
 import os
 import sys
 from sklearn.datasets import load_iris
-
-import warnings
-warnings.filterwarnings("ignore")
 
 import pandas as pd
 import seaborn  as sb
@@ -25,31 +24,36 @@ import warnings
 warnings.filterwarnings("ignore")
 
 import numpy as np
-import sklearn
 from scipy import stats
-from scipy.stats import kurtosis,skew
-from pandas.plotting import scatter_matrix
+import sklearn
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
-from scipy.stats import chi2_contingency
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import KFold
-
+#-----------plotting
 import plotly.graph_objs as go
 import plotly.offline as py
+from pandas.plotting import scatter_matrix
 
-from sklearn.svm import SVC
+
+#-------------------For Algorithms
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
-
-from sklearn.metrics import roc_curve,roc_auc_score
+from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVC
+
+#---------scoring
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import roc_curve,roc_auc_score
+#-----Utilities
+from scipy.stats import kurtosis,skew
+from scipy.stats import chi2_contingency
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+
+from sklearn.preprocessing import LabelEncoder
 
 from imblearn.combine import SMOTETomek#combine undersampling and oversampling
-from sklearn.ensemble import RandomForestClassifier
-
